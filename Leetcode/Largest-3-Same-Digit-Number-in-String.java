5c8042f26eae597b6a53d39af604f0ec923a6c8d
Prateek Maheshwari <friskycodeur@gmail.com>
2025-08-14 18:31:14 +0530

    [2264] [Time Beats: 81.68%] [Memory Beats: 92.25%] - LeetPush

--- Leetcode/Largest-3-Same-Digit-Number-in-String.java
+++ Leetcode/Largest-3-Same-Digit-Number-in-String.java
@@ -1,32 +1,15 @@
 class Solution {
     public String largestGoodInteger(String num) {
         if(num==null || num.isEmpty()) return "";
-        char currChar = '/', maxChar = '/';
-        int sz = 0;
-        String ans = "";
-        for(char ch: num.toCharArray()){
-            // check if char is latest or not
-            if(ch == currChar){
-            // if yes size++
-                sz++;
-            } else {
-                // if not, check size so far
-                // if size >= 3, then check if currChar > maxChar, if yes populate maxChar by currChar
-                // now set size to 1 and currChar to num.charAt(i)
-
-                if(sz>=3 && currChar > maxChar){
-                    maxChar = currChar;
-                }
-                currChar = ch;
-                sz = 1;
-         
-           }
+        char maxChar = Character.MIN_VALUE;
+        for(int i=0; i< num.length()-2;i++){
+            char currChar = num.charAt(i);
+            if(currChar == num.charAt(i+1) && currChar == num.charAt(i+2)){
+                if(currChar>maxChar) maxChar = currChar;
+                if(maxChar == '9') break; 
+            }
         }
 
-        if(sz>=3 && currChar > maxChar){
-            maxChar = currChar;
-        }
-
-        return maxChar == '/' ? ans : ans + String.valueOf(maxChar).repeat(3);
+        return maxChar > Character.MIN_VALUE ? String.valueOf(maxChar).repeat(3) : "";
     }
 }
